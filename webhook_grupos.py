85c2282795bc4f7f3e3255a9301b060fdba8a83b
perarnaupol-ux <perarnaupol@gmail.com>
2025-09-04 11:43:45 +0200

    Add files via upload

--- webhook_grupos.py
+++ webhook_grupos.py
@@ -238,4 +238,6 @@ def webhook():
     return jsonify({"text": respuesta})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
